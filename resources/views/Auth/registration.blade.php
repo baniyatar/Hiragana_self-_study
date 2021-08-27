df8b801107b96f35cc2610d2b4aa50f8d9c9b873
baniyatar <48229862+baniyatar@users.noreply.github.com>
2021-08-27 18:45:17 +0900

    hiragana Project

---
+++ resources/views/Auth/registration.blade.php
@@ -0,0 +1,110 @@
+@extends('layout')
+  
+@section('content')
+<main class="login-form">
+  <div class="cotainer">
+      <div class="row justify-content-center">
+          <div class="col-md-8">
+              <div class="card">
+                  <div class="card-header">Register</div>
+                  <div class="card-body">
+  
+                      <form action="{{ route('register.post') }}" method="POST">
+                          @csrf
+                          <div class="form-group row">
+                              <label for="name" class="col-md-4 col-form-label text-md-right">Username</label>
+                              <div class="col-md-6">
+                                  <input type="text" id="name" class="form-control" name="name" required autofocus>
+                                  @if ($errors->has('name'))
+                                      <span class="text-danger">{{ $errors->first('name') }}</span>
+                                  @endif
+                              </div>
+                          </div>
+  
+                        
+  
+                          <div class="form-group row">
+                              <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
+                              <div class="col-md-6">
+                                  <input type="password" id="password" class="form-control" name="password" required>
+                                  @if ($errors->has('password'))
+                                      <span class="text-danger">{{ $errors->first('password') }}</span>
+                                  @endif
+                              </div>
+                          </div>
+
+                          <div class="form-group row">
+                            <label for="email_address" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
+                            <div class="col-md-6">
+                                <input type="text" id="email_address" class="form-control" name="email" required autofocus>
+                                @if ($errors->has('email'))
+                                    <span class="text-danger">{{ $errors->first('email') }}</span>
+                                @endif
+                            </div>
+                        </div>
+  
+                        <div class="form-group row">
+                            <label for="firstname" class="col-md-4 col-form-label text-md-right">Firstname</label>
+                            <div class="col-md-6">
+                                <input type="text" id="firstname" class="form-control" name="firstname" required autofocus>
+                                @if ($errors->has('firstname'))
+                                    <span class="text-danger">{{ $errors->first('firstname') }}</span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="surname" class="col-md-4 col-form-label text-md-right">Surname</label>
+                            <div class="col-md-6">
+                                <input type="text" id="surname" class="form-control" name="surname" required autofocus>
+                                @if ($errors->has('surname'))
+                                    <span class="text-danger">{{ $errors->first('surname') }}</span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="city" class="col-md-4 col-form-label text-md-right">City/town</label>
+                            <div class="col-md-6">
+                                <input type="text" id="city" class="form-control" name="city" required autofocus>
+                                @if ($errors->has('city'))
+                                    <span class="text-danger">{{ $errors->first('city') }}</span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="country" class="col-md-4 col-form-label text-md-right">Country</label>
+                            <div class="col-md-6">
+                                <input type="text" id="country" class="form-control" name="country" required autofocus>
+                                @if ($errors->has('country'))
+                                    <span class="text-danger">{{ $errors->first('country') }}</span>
+                                @endif
+                            </div>
+                        </div>
+
+
+                          <div class="form-group row">
+                              <div class="col-md-6 offset-md-4">
+                                  <div class="checkbox">
+                                      <label>
+                                          <input type="checkbox" name="remember"> Remember Me
+                                      </label>
+                                  </div>
+                              </div>
+                          </div>
+  
+                          <div class="col-md-6 offset-md-4">
+                              <button type="submit" class="btn btn-primary">
+                                  Register
+                              </button>
+                          </div>
+                      </form>
+                        
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</main>
+@endsection
