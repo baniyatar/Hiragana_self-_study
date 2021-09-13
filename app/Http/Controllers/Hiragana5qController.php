6473ea948a37f1f6ac8ab95ead998fd70f2ee16e
baniyatar <48229862+baniyatar@users.noreply.github.com>
2021-09-13 10:56:34 +0900

    hiragana Content

--- app/Http/Controllers/Hiragana5qController.php
+++ app/Http/Controllers/Hiragana5qController.php
@@ -7,76 +7,76 @@ use Illuminate\Http\Request;
 class Hiragana5qController extends Controller
 {
  
-    // public function index()
-    // {
-    //     $Users = User::latest()->paginate(5);
+    public function index()
+    {
+        $Users = User::latest()->paginate(5);
 
-    //     return view('Users.index',compact('Users'))
-    //         ->with('i', (request()->input('page', 1) - 1) * 5);
-    // }
+        return view('Users.index',compact('Users'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
 
   
-    // public function create()
-    // {
-    //     return view('Users.create');
-    // }
+    public function create()
+    {
+        return view('Users.create');
+    }
 
     
-    // public function store(Request $request)
-    // {
-    //     $request->validate([
-    //         'username' => 'required',
-    //         'password' => 'required|min:6',
-    //         'email' => 'required|email|unique:users',
-    //         'username' => 'required',
-    //         'surname' => 'required',
-    //         'city' => 'required',
-    //         'country' => 'required',
-    //         'payment'=>'required',
-    //     ]);
+    public function store(Request $request)
+    {
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required|min:6',
+            'email' => 'required|email|unique:users',
+            'username' => 'required',
+            'surname' => 'required',
+            'city' => 'required',
+            'country' => 'required',
+            'payment'=>'required',
+        ]);
 
-    //     User::create($request->all());
+        User::create($request->all());
 
-    //     return redirect()->route('Users.index')
-    //         ->with('success','User created successfully.');
-    // }
+        return redirect()->route('Users.index')
+            ->with('success','User created successfully.');
+    }
 
    
-    // public function show(User $User)
-    // {
-    //     return view('Users.showRegistration',compact('User'));
-    // }
+    public function show(User $User)
+    {
+        return view('Users.showRegistration',compact('User'));
+    }
 
     
-    // public function edit(User $User)
-    // {
-    //     return view('Users.editRegistration',compact('User'));
-    // }
+    public function edit(User $User)
+    {
+        return view('Users.editRegistration',compact('User'));
+    }
 
    
-    // public function update(Request $request, User $User)
-    // {
-    //     $request->validate([
-    //         'username' => 'required',
-    //         'password' => 'required|min:6',
-    //         'email' => 'required|email|unique:users',
-    //         'username' => 'required',
-    //         'surname' => 'required',
-    //         'city' => 'required',
-    //         'country' => 'required',
-    //     ]);
+    public function update(Request $request, User $User)
+    {
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required|min:6',
+            'email' => 'required|email|unique:users',
+            'username' => 'required',
+            'surname' => 'required',
+            'city' => 'required',
+            'country' => 'required',
+        ]);
 
-    //     $User->update($request->all());
+        $User->update($request->all());
 
-    //     return redirect()->route('Users.index')
-    //         ->with('success','User updated successfully');
-    // }
+        return redirect()->route('Users.index')
+            ->with('success','User updated successfully');
+    }
 
-    // public function destroy(User $User)
-    // {
-    //     $User->delete();
+    public function destroy(User $User)
+    {
+        $User->delete();
 
-    //     return redirect()->route('Users.index')
-    //         ->with('success','User deleted successfully');
-    // }
+        return redirect()->route('Users.index')
+            ->with('success','User deleted successfully');
+    }
 }
