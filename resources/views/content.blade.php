2cb89cd3f517a8eab4cdfb1d2b8f896b142a907b
baniyatar <48229862+baniyatar@users.noreply.github.com>
2021-10-19 16:37:02 +0900

    resolve content inside

--- resources/views/content.blade.php
+++ resources/views/content.blade.php
@@ -6,23 +6,29 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+
+<script>
+    function resizeIframe(obj) {
+    obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
+  }
+</script>
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Hiragana') }}</div>
 
+            </div>
 
-
-<iframe width="100%" height="100%"  controls id="myvideo"
+<iframe width="100%" height="100%" scrolling="no" onload="resizeIframe(this)" controls id="myvideo"
    src="\hiragana\Hiragana Self Study solve (Published)\index.html">
 
   </iframe>
   <button onclick="openFullscreen();">Click here for Fullscreen Mode</button>
   <p><strong>Tip:</strong> Press the "Esc" key to exit full screen.</p>
- 
-  <a class="nav-link" href="{{ route('logout') }}">やめます。 Logout</a> </div>
-</div>
+  <button type="button"　href="{{ route('login') }}" class="btn btn-Secondary btn-lg btn-block"> やります　quit  </button> </div>
+
         </div></div>
        
 <script>
