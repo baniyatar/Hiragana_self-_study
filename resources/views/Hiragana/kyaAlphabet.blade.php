e78277852f606b5cc3e185d96105b1b46b5cc7fe
Kanemiyo <fjysykk@gmail.com>
2021-11-18 11:15:55 +0900

    update kya-gyo button

--- resources/views/Hiragana/kyaAlphabet.blade.php
+++ resources/views/Hiragana/kyaAlphabet.blade.php
@@ -61,7 +61,7 @@
 
 	</div>
 	<button class="home_btn" onclick="location.href='{{ url('/home')}}'">HOME</button>
-	<button class="prev_btn" onclick="location.href='{{ url('/nAlphabet')}}'">&lt;PREV</button>
+	<button class="prev_btn" onclick="location.href='{{ url('/contractedConstruct')}}'">&lt;PREV</button>
 	<button class="next_btn" onClick="location.href='{{ url('/kyuAlphabet')}}'">NEXT&gt;</button>
 </body>
 </html>
